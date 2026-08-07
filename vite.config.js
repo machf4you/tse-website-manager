17052a033ef8f2c6745c5a9f43b6a1ea0559409d
Mac McCarthy <macone157@gmail.com>
2026-08-07 05:31:14 +0100

    Deployment pipeline verification test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Deployment verification test: 2026-08-07
 export default defineConfig({
   plugins: [react()],
 })
