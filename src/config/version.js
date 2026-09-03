13a38508ccbaddd666aa660e8eabd95def3983d6
Mac McCarthy <macone157@gmail.com>
2026-09-03 17:40:34 +0200

    style(apps-dashboard): widen layout to 1680px, 3-column cards on desktop, prevent name truncation, bump to v2.04

--- src/config/version.js
+++ src/config/version.js
@@ -1,7 +1,7 @@
-export const CURRENT_BUILD_VERSION = '2.03'
-export const CURRENT_BUILD_LABEL = 'V2.03 | READY'
-export const CURRENT_BUILD_HASH = 'tseappsdash203'
-export const CURRENT_BUILD_TIMESTAMP = 1788370000000
+export const CURRENT_BUILD_VERSION = '2.04'
+export const CURRENT_BUILD_LABEL = 'V2.04 | READY'
+export const CURRENT_BUILD_HASH = 'appsgridwide204'
+export const CURRENT_BUILD_TIMESTAMP = 1788380000000
 
 
 
