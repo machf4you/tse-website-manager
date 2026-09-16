3c92e2d653996729ac2658b3bdb2a31c4af86710
Mac McCarthy <macone157@gmail.com>
2026-09-16 10:28:48 +0200

    chore: version bump post-build

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.49'
 export const CURRENT_BUILD_LABEL = 'V2.49 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mu3u9fsf-0ibag'
-export const CURRENT_BUILD_TIMESTAMP = 1789547307567
+export const CURRENT_BUILD_HASH = 'wm-mu3u9rae-klld6'
+export const CURRENT_BUILD_TIMESTAMP = 1789547322470
