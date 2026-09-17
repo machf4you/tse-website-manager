5460c49331f485e671b902781569406a65fd37e0
Mac McCarthy <macone157@gmail.com>
2026-09-17 16:25:19 +0200

    style: remove internal vertical scrollbar and allow natural expansion for generated article content

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mu5lci50-xu0bd'
-export const CURRENT_BUILD_TIMESTAMP = 1789653266388
+export const CURRENT_BUILD_HASH = 'wm-mu5mg34u-8ea8o'
+export const CURRENT_BUILD_TIMESTAMP = 1789655113182
