e820b80500f3244f1b6ebb2ecc7f0a79f2bbc450
Mac McCarthy <macone157@gmail.com>
2026-09-17 15:22:10 +0200

    build: bundle production dist for article automation

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mu5k6jfz-8v09i'
-export const CURRENT_BUILD_TIMESTAMP = 1789651308527
+export const CURRENT_BUILD_HASH = 'wm-mu5k6w6o-2xq9v'
+export const CURRENT_BUILD_TIMESTAMP = 1789651325040
