2e791ec2b231ddb2690b6e7e04a8601e114714d0
Mac McCarthy <macone157@gmail.com>
2026-09-17 16:56:55 +0200

    chore: update build artifacts

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mu5nkgpr-4vtr8'
-export const CURRENT_BUILD_TIMESTAMP = 1789656997023
+export const CURRENT_BUILD_HASH = 'wm-mu5nksce-pu27g'
+export const CURRENT_BUILD_TIMESTAMP = 1789657012094
