1b79deb8340216daaabcd8c006a1271b70e3eea1
Mac McCarthy <macone157@gmail.com>
2026-09-18 16:19:37 +0200

    Build update after batch runner fix

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mu71o502-fcz7x'
-export const CURRENT_BUILD_TIMESTAMP = 1789741149266
+export const CURRENT_BUILD_HASH = 'wm-mu71ogva-1ibvo'
+export const CURRENT_BUILD_TIMESTAMP = 1789741164646
