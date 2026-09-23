26c02ad0456fb0181c2c63cbebf9dcad4026e616
Mac McCarthy <macone157@gmail.com>
2026-09-23 17:11:44 +0200

    chore(version): record build hash wm-mue8pyep-jeq8v for V2.52

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mue8o8po-a7af9'
-export const CURRENT_BUILD_TIMESTAMP = 1790176174620
+export const CURRENT_BUILD_HASH = 'wm-mue8pyep-jeq8v'
+export const CURRENT_BUILD_TIMESTAMP = 1790176254577
