6159ee5818b037ba80937d5d70417d663dc7e0ae
Mac McCarthy <macone157@gmail.com>
2026-09-23 17:56:20 +0200

    chore(version): record build hash wm-mueac9lc-gxmr5 for V2.52

--- src/config/version.js
+++ src/config/version.js
@@ -1,4 +1,4 @@
 export const CURRENT_BUILD_VERSION = '2.52'
 export const CURRENT_BUILD_LABEL = 'V2.52 | READY'
-export const CURRENT_BUILD_HASH = 'wm-mueabt71-gmmkl'
-export const CURRENT_BUILD_TIMESTAMP = 1790178953869
+export const CURRENT_BUILD_HASH = 'wm-mueac9lc-gxmr5'
+export const CURRENT_BUILD_TIMESTAMP = 1790178975120
